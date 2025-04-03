d7de1cc4c5ac96d1502ff339f22fbe170b578011
Aryaman Jeendgar <jeendgararyaman@gmail.com>
2025-04-03 21:03:21 +0530

    training works

--- models/mlp.py
+++ models/mlp.py
@@ -21,17 +21,21 @@ class MLP_curve(nn.Module):
     def __init__(self, num_classes: int, fix_points: list[bool]):
         super(MLP_curve, self).__init__()
         self.flatten = nn.Flatten()
-        self.layers = nn.Sequential(
-            curves.Linear(28 * 28, 128, fix_points=fix_points),
-            nn.ReLU(),
-            curves.Linear(128, 64, fix_points=fix_points),
-            nn.ReLU(),
-            curves.Linear(64, num_classes, fix_points=fix_points)
-        )
+        self.fc1 = curves.Linear(28 * 28, 128, fix_points=fix_points)
+        self.relu1 = nn.ReLU()
+        self.fc2 = curves.Linear(128, 64, fix_points=fix_points)
+        self.relu2 = nn.ReLU()
+        self.fc3 = curves.Linear(64, num_classes, fix_points=fix_points)
 
-    def forward(self, x):
+    def forward(self, x, coeffs_t):
         x = self.flatten(x)
-        return self.layers(x)
+        x = self.fc1(x, coeffs_t)
+        x = self.relu1(x)
+        x = self.fc2(x, coeffs_t)
+        x = self.relu2(x)
+        x = self.fc3(x, coeffs_t)
+
+        return x
 
 
 class MLP:
